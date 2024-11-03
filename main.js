b82d9e5a0936b91c065955e4674bc1270414f8b3
vdhruvannaidu <vdhruvannaidu@gmail.com>
2024-11-04 02:38:57 +0530

    mdal-autoplay

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const progressCircle = document.querySelector(".autoplay-progress svg");
     var swiper = new Swiper(".mySwiper", {
       spaceBetween: 30,
       centeredSlides: true,
-      
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true
